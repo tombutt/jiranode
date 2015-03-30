7bc1c3ca8921039b7894e944f87e15f3a79fd73d
Tom Butt <tbutt@redhat.com>
2015-03-30 06:30:34 -0600

    openshift test

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var release = require('./release.js')
 
 var app = express()
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8888;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
 app.get("/", function(req, res) {
     res.send('hello world')
 })
@@ -14,7 +17,7 @@ app.get("/release/:project/:number", function(req, res){
     }  
 })
 
-var server = app.listen(8888, function() {
+var server = app.listen(server_port, server_ip_address, function() {
     var host = server.address().address
     var port = server.address().port
     console.log('Example app listening at http://%s:%s', host, port)
